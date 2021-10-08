7dbf44550a19db43b93d550bd1df931874bccb09
Gematik <software-development@gematik.de>
2021-10-08 13:45:08 +0000

    update for Gradle 7

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"de.gematik.ti.cardreader.access","l":"CardReaderControllerManager"},{"p":"de.gematik.ti.cardreader.access","l":"ICardReaderControllerInitializer"}]
